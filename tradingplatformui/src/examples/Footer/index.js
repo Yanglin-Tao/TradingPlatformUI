4edff95ab5c5cb757552311db7eacbc069d404d2
Yanglin-Tao <81639920+Yanglin-Tao@users.noreply.github.com>
2024-07-25 12:00:16 -0400

    Remove footer and excess features from sign-in template

--- tradingplatformui/src/examples/Footer/index.js
+++ tradingplatformui/src/examples/Footer/index.js
@@ -18,18 +18,12 @@ import PropTypes from "prop-types";
 
 // @mui material components
 import Link from "@mui/material/Link";
-import Icon from "@mui/material/Icon";
 
 // Material Dashboard 2 React components
 import MDBox from "components/MDBox";
 import MDTypography from "components/MDTypography";
 
-// Material Dashboard 2 React base styles
-import typography from "assets/theme/base/typography";
-
 function Footer({ company, links }) {
-  const { href, name } = company;
-  const { size } = typography;
 
   const renderLinks = () =>
     links.map((link) => (
@@ -52,29 +46,6 @@ function Footer({ company, links }) {
       px={1.5}
     >
       <MDBox
-        display="flex"
-        justifyContent="center"
-        alignItems="center"
-        flexWrap="wrap"
-        color="text"
-        fontSize={size.sm}
-        px={1.5}
-      >
-        &copy; {new Date().getFullYear()}, made with
-        <MDBox fontSize={size.md} color="text" mb={-0.5} mx={0.25}>
-          <Icon color="inherit" fontSize="inherit">
-            favorite
-          </Icon>
-        </MDBox>
-        by
-        <Link href={href} target="_blank">
-          <MDTypography variant="button" fontWeight="medium">
-            &nbsp;{name}&nbsp;
-          </MDTypography>
-        </Link>
-        for a better web.
-      </MDBox>
-      <MDBox
         component="ul"
         sx={({ breakpoints }) => ({
           display: "flex",
@@ -92,7 +63,7 @@ function Footer({ company, links }) {
         })}
       >
         {renderLinks()}
-      </MDBox>
+      </MDBox> 
     </MDBox>
   );
 }
--- tradingplatformui/src/routes.js
+++ tradingplatformui/src/routes.js
@@ -40,6 +40,8 @@ import Dashboard from "layouts/dashboard";
 import Tables from "layouts/tables";
 import Billing from "layouts/billing";
 import Profile from "layouts/profile";
+import SignIn from "layouts/authentication/sign-in";
+import SignUp from "layouts/authentication/sign-up";
 
 // @mui icons
 import Icon from "@mui/material/Icon";
@@ -76,6 +78,22 @@ const routes = [
     icon: <Icon fontSize="small">table_view</Icon>,
     route: "/tables",
     component: <Tables />,
+  },
+  {
+    type: "collapse",
+    name: "Sign In",
+    key: "sign-in",
+    icon: <Icon fontSize="small">login</Icon>,
+    route: "/authentication/sign-in",
+    component: <SignIn />,
+  },
+  {
+    type: "collapse",
+    name: "Sign Up",
+    key: "sign-up",
+    icon: <Icon fontSize="small">assignment</Icon>,
+    route: "/authentication/sign-up",
+    component: <SignUp />,
   }
 ];
 
