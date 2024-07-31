d83e908b89967b513b033fb95af41980aa38dcdd
yanglin_tao <81639920+Yanglin-Tao@users.noreply.github.com>
2024-07-31 00:26:10 -0400

    Integrate order history and calculate daily spread

--- tradingplatformui/src/layouts/billing/components/Invoice/index.js
+++ tradingplatformui/src/layouts/billing/components/Invoice/index.js
@@ -23,7 +23,7 @@ import Icon from "@mui/material/Icon";
 import MDBox from "components/MDBox";
 import MDTypography from "components/MDTypography";
 
-function Invoice({ date, id, price, noGutter }) {
+function Invoice({ date, price, noGutter }) {
   return (
     <MDBox
       component="li"
@@ -38,20 +38,11 @@ function Invoice({ date, id, price, noGutter }) {
         <MDTypography display="block" variant="button" fontWeight="medium">
           {date}
         </MDTypography>
-        <MDTypography variant="caption" fontWeight="regular" color="text">
-          {id}
-        </MDTypography>
       </MDBox>
       <MDBox display="flex" alignItems="center">
         <MDTypography variant="button" fontWeight="regular" color="text">
-          {price}
+          Total: {price}
         </MDTypography>
-        <MDBox display="flex" alignItems="center" lineHeight={1} ml={3} sx={{ cursor: "pointer" }}>
-          <Icon fontSize="small">picture_as_pdf</Icon>
-          <MDTypography variant="button" fontWeight="bold">
-            &nbsp;PDF
-          </MDTypography>
-        </MDBox>
       </MDBox>
     </MDBox>
   );
